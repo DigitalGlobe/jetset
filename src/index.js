5dc6db7d41c066d8bbfe9bf6d12fa2bbf5b93fab
glortho <jed.verity@digitalglobe.com>
2017-05-03 21:46:58 -0400

    add TreeViewer as export from index

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export default from './api';
 export { default as store } from './store';
 export { default as container } from './container';
 export { default as subscribe } from './subscribe';
+export { default as TreeViewer } from './tree_viewer';
