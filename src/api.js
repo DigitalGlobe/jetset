7c4dc6c9b02c27012ef46fa054bbc6b3186b909a
glortho <jed.verity@digitalglobe.com>
2017-05-10 14:09:58 -0400

    fix fetch reference

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { List, Map } from 'immutable';
-import initFetch from 'fetch';
+import initFetch from 'iso-fetch-stream';
 
 import { isSchema, getIdField } from './lib/schema';
 import store from './store';
