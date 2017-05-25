f9a0a44f4bc1a27ab12f46bb0d515eb9fe72008a
glortho <jed.verity@digitalglobe.com>
2017-05-25 11:12:44 -0400

    empty space clean-up

--- src/api/decorator.js
+++ src/api/decorator.js
@@ -16,5 +16,3 @@ export default apiProps =>
       ? React.createElement( Api, apiProps, createChildren( children, maybeComponent ) )
       : ApiDecorator.bind( null, [].concat( children, maybeComponent || [] ) );
   };
-
-
