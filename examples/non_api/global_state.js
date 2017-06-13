dbe38b14d503d3e5365d281b55346e9a8b4359cc
glortho <jed.verity@digitalglobe.com>
2017-06-13 13:26:40 -0600

    revert example change

--- examples/non_api/global_state.js
+++ examples/non_api/global_state.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { globalState } from '../../src/index';
+import { globalState } from '../../src/subscribe';
 
 // TODO: we'll need state naming convention + clear way to subscribe to nested
 // state
-@globalState('exampleWithNoInitialState')
+@globalState({ example: 'foo', example2: 'bar' }, 'exampleWithNoInitialState')
 class GlobalStateExample extends React.Component {
   render() {
     return (
       <div>
+        <div>
+          <span>example state: { this.props.example.get() }</span>
+          <button onClick={() => this.props.example.set( 'foo' )}>Set to foo</button>
+          <button onClick={() => this.props.example.set( 'bar' )}>Set to bar</button>
+        </div>
+        <div>
+          <span>example2 state: { this.props.example2.get() }</span>
+          <button onClick={() => this.props.example2.set( 'foo' )}>Set to foo</button>
+          <button onClick={() => this.props.example2.set( 'bar' )}>Set to bar</button>
+        </div>
         <div>
           <span>exampleWithNoInitialState: { this.props.exampleWithNoInitialState.get() || 'n/a'}</span>
           <button onClick={() => this.props.exampleWithNoInitialState.set( 'foo' )}>Set to foo</button>
